dfe03deff17d74bf6adf972b0e5ec06acaec6d79
Lance Gatlin <lance.gatlin@gtri.gatech.edu>
2012-11-14 06:13:08 -0500

    added copy license

--- src/main/java/org/gtri/util/xsddatatypes/XsdToken.java
+++ src/main/java/org/gtri/util/xsddatatypes/XsdToken.java
@@ -1,7 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    Copyright 2012 Georgia Tech Research Institute
+
+    Author: lance.gatlin@gtri.gatech.edu
+
+    This file is part of org.gtri.util.xsddatatypes library.
+
+    org.gtri.util.xsddatatypes library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    org.gtri.util.xsddatatypes library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with org.gtri.util.xsddatatypes library. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package org.gtri.util.xsddatatypes;
 
 /**
